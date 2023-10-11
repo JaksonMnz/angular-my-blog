149a0eca23a46e042dc5e7671529e6dede18c5c8
JaksonMnz <jaksonmenezes96@gmail.com>
2023-10-11 03:28:28 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,6 +12,10 @@ export class SmallCardComponent implements OnInit {
 
   @Input()
   cardTitle:string =""
+
+  @Input()
+  id:string = "0"
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D!",
+    "discription": "Marvel anuncia no filme do homem de ferro.",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "NOVA SERIE ANUCIADA NO DISNEY+",
+    "discription": "bla bla bla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+]
